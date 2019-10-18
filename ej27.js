adedc66a2126fb7a374aa1c66ce3e3647e5b20b4
/SabrinaRey <sabrinareynares@gmail.com>
2019-10-18 20:00:22 -0300

    primer commit

---
+++ ej27.js
@@ -0,0 +1,59 @@
+let numeroUno = 2;
+
+let numeroDos = 3;
+
+let numero = 9;
+
+let cuadrado = 10;
+
+let area = 5;
+
+let ladoUno = 11;
+
+let ladoDos = 20;
+
+let ladoTres =6;
+
+let altura = 12;
+
+let base =4;
+
+console.log(numeroUno+numeroDos);
+
+console.log(numeroUno-numeroDos);
+
+console.log(numeroUno*numeroDos);
+
+console.log(numeroUno/numeroDos);
+
+console.log(numeroUno%numeroDos);
+
+console.log(numero*1);
+
+console.log(numero*2);
+
+console.log(numero*3);
+
+console.log(numero*4);
+
+console.log(numero*5);
+
+console.log(numero*6);
+
+console.log(numero*7);
+
+console.log(numero*8);
+
+console.log(numero*9);
+
+console.log(numero*10);
+
+console.log(cuadrado*4);
+
+console.log(area*area);
+
+console.log(ladoUno+ladoDos+ladoTres);
+
+console.log(altura+base);
+
+console.log(base*altura);
